6bf5ca3ddf95686b2568162102671ce905eb9ea9
aurefsec <auref.contact@protonmail.com>
2026-03-19 20:49:36 +0100

    fixed

--- libasm.h
+++ libasm.h
@@ -6,6 +6,6 @@
 # include <string.h>
 
 size_t  ft_strlen(const char* s);
-char*   ft_strcpy(char* restrict dst, char* restrict const src)
+char*   ft_strcpy(char* restrict dst, const char* restrict src)
 
 #endif
