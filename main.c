135730b2baf5e35bc1c776401ae6c89bc1aa6f12
aurefsec <auref.contact@protonmail.com>
2026-04-12 20:49:32 +0200

    fixed

--- main.c
+++ main.c
@@ -29,15 +29,20 @@ int main(void)
   printf("write = %zd\nft_write = %zd\n", w1, w2);
 
   printf("\nft_read :\n");
-  int fd[2];
-  pipe(fd);
-  write(fd[1], "je fais un test\n", 16);
+  int fd1[2];
+  int fd2[2];
+  pipe(fd1);
+  pipe(fd2);
+  write(fd1[1], "je fais un test\n", 16);
+  write(fd2[1], "je fais un test\n", 16);
   char buff1[50];
   char buff2[50];
-  ssize_t r1 = read(fd[0], buff1, 16);
-  ssize_t r2 = ft_read(fd[0], buff2, 16);
+  ssize_t r1 = read(fd1[0], buff1, 16);
+  ssize_t r2 = ft_read(fd2[0], buff2, 16);
   printf("read return = %zd\nread buff = %s\n", r1, buff1);
   printf("ft_read return = %zd\nft_read buff = %s\n", r2, buff2);
+  close(fd1);
+  close(fd2);
 
   return 0;
 }
