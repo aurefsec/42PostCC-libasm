92c7d5823ca24fea7bfe112dd8e47a5973ddc756
aurefsec <auref.contact@protonmail.com>
2026-04-12 19:55:44 +0200

    fixed

--- libasm.h
+++ libasm.h
@@ -10,7 +10,7 @@
 size_t  ft_strlen(const char* s);
 char*   ft_strcpy(char* restrict dst, const char* restrict src);
 int     ft_strcmp(const char* s1, const char* s2);
-ssize_t ft_write(int fd, const void buf[.count], size_t count);
+ssize_t ft_write(int fd, const void* buf, size_t count);
 
 
 #endif
