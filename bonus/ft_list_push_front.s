ba6b40c2900253b400b3647f28d92b8673cebbf1
aurefsec <auref.contact@protonmail.com>
2026-04-19 01:06:04 +0200

    ft_list_push_front

--- bonus/ft_list_push_front.s
+++ bonus/ft_list_push_front.s
@@ -1,4 +1,31 @@
 section .text
 global ft_list_push_front
+extern malloc
 
 ft_list_push_front:
+  push rdi
+  mov rdi, 16
+
+  call malloc wrt ..plt
+  test rax, rax
+  je .error
+
+  mov rdi, QWORD [rsi]
+  mov QWORD [rax], rdi
+
+  pop rdi
+  mov rdx, QWORD [rdi]
+  test rdx, rdx
+  jne .retrive_next
+  jump .end
+
+.retrive_next:
+  mov rdx, QWORD [rdx + 8]
+  mov QWORD [rax + 8], rdx
+
+.end:
+  mov QWORD [rdi], rax
+  ret
+
+.error:
+  ret
