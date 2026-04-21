5c343e041107acad8621235467459f635156dd83
aurefsec <auref.contact@protonmail.com>
2026-04-22 00:04:57 +0200

    ft_list_sort

--- bonus/ft_list_sort.s
+++ bonus/ft_list_sort.s
@@ -4,17 +4,18 @@ global ft_list_sort
 ft_list_sort:
   push rbx
   mov rbx, rsi
-
-  push r8
   mov r8, rdi
+  jmp .prepare
 
-  mov rdx, QWORD [rdi]
+.prepare:
+  xor r9, r9
+  push r8
+  mov rdx, QWORD [r8]
   test rdx, rdx
   je .end
+  jmp .loop
 
-  xor r9, r9
-
-  .loop:
+.loop:
   mov rcx, rdx
   mov rdx, QWORD[rdx + 8]
   test rdx, rdx
@@ -45,15 +46,9 @@ ft_list_sort:
   mov QWORD [rdx], r10
   jmp .loop
 
-.unsorted:
-  xor r9, r9
-  mov rdx, QWORD [r8]
-  push r8
-  jmp .loop
-
 .end:
   pop r8
   test r9, r9
-  jne .unsorted
+  jne .prepare
   pop rbx
   ret
